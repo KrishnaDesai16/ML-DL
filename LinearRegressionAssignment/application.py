c2eb38be2a1ceb28309066c1bae0cc38ebd9ce4b
Krishna Desai <58232684+KrishnaDesai16@users.noreply.github.com>
2021-01-26 21:34:06 +0530

    code added

---
+++ LinearRegressionAssignment/application.py
@@ -0,0 +1,49 @@
+
+# importing the necessary dependencies
+from flask import Flask, render_template, request
+from flask_cors import cross_origin
+import pickle
+
+application = Flask(__name__) # initializing a flask app
+# app=application
+@application.route('/',methods=['GET'])  # route to display the home page
+@cross_origin()
+def homePage():
+    return render_template("index.html")
+
+@application.route('/predict',methods=['POST','GET']) # route to show the predictions in a web UI
+@cross_origin()
+def index():
+    if request.method == 'POST':
+        try:
+            #  reading the inputs given by the user
+            CRIM = float(request.form['CRIM'])
+            ZN = float(request.form['ZN'])
+            INDUS = float(request.form['INDUS'])
+            NOX = float(request.form['NOX'])
+            RM = float(request.form['RM'])
+            AGE = float(request.form['AGE'])
+            DIS = float(request.form['DIS'])
+            TAX = float(request.form['TAX'])
+            PTRATIO = float(request.form['PTRATIO'])
+            B = float(request.form['B'])
+            LSTAT = float(request.form['LSTAT'])
+            filename = 'finalized_model.pickle'
+            loaded_model = pickle.load(open(filename, 'rb')) # loading the model file from the storage
+            # predictions using the loaded model file
+            prediction=loaded_model.predict([[CRIM,ZN,INDUS,NOX,RM,AGE,DIS,TAX,PTRATIO,B,LSTAT]])
+            print('prediction is', prediction)
+            # showing the prediction results in a UI
+            return render_template('results.html',prediction=round(100*prediction[0]))
+        except Exception as e:
+            print('The Exception message is: ',e)
+            return 'something is wrong'
+    # return render_template('results.html')
+    else:
+        return render_template('index.html')
+
+
+
+if __name__ == "__main__":
+    #app.run(host='127.0.0.1', port=8001, debug=True)
+	application.run(debug=True) # running the app
